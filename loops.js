c823e7435b51216af6f76817eb8ed8b1343cd36c
Norathat Jongjitnant <norathat@gmail.com>
2020-09-13 13:33:25 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i < 2 ? '' : 's'}.`)
+  }
+  return array
+}
+
+function whileLoop(n){
+  while (n >= -1) {
+    if (n >= 0) {
+      console.log(n)
+      n -= 1
+    } else if (n < 0) {
+      console.log('done')
+    }
+  }
+}
