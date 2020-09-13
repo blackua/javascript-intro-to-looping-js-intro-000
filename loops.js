12754a96affb873f2ea18a7fcc05bcc440309e74
Norathat Jongjitnant <norathat@gmail.com>
2020-09-13 13:45:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,16 @@ function whileLoop(n){
   n -= 1
   }
 }
+
+let i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(num){
+  do {
+    console.log("I run once regardless");
+  } while (incrementVariable() < num);
+}
